abdacb5298625f58225d328d01cd048e29462e23
Orian <orianhit@hit.ac.il>
2022-11-06 08:45:15 +0200

    changes

--- source/app/conf/development/settings.py
+++ source/app/conf/development/settings.py
@@ -95,8 +95,12 @@ EMAIL_HOST_PASSWORD = ''
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'db',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
