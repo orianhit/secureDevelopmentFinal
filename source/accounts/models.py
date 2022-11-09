ab78a1ac9607c2d4f5308c02d4165089590fefc2
Orian <orianhit@hit.ac.il>
2022-11-09 20:38:32 +0200

    add customer model

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=42)
+    email = models.EmailField(max_length=75)
